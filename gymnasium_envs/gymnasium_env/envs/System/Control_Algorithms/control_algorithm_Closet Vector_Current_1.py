a43f8cc591f2fc943a794987f7b2a0fbcc82d2d1
MuhammadUsamaSattar <usamasattar.3347@gmail.com>
2025-01-03 18:21:53 +0200

    Missed 'System' folder in previous commit

---
+++ gymnasium_envs/gymnasium_env/envs/System/Control_Algorithms/control_algorithm_Closet Vector_Current_1.py
@@ -0,0 +1,33 @@
+import math
+
+
+def get_coil_vals(particle_loc, goal_loc, coil_vals, coil_locs):
+    """Calculates the current values for given particle location, goal location and coil values
+
+    Args:
+        particle_loc (list): List of size 2 containing x and y co-ordinates of particle location
+        goal_loc (list): List of size 2 containing x and y co-ordinates of goal location
+        coil_vals (list): List of size 8 containing scaled current values where 0th element corresponds to Northern coil
+        coil_locs (list): List of size 8 containing coil locations where 0th element corresponds to Northern coil
+
+    Returns:
+        list: List of size 8 containing scaled current values where 0th element corresponds to Northern coil
+    """
+    angle = math.pi
+    min_coil = -1
+
+    for i in range(len(coil_vals)):
+        coil_vals[i] = 0
+        alpha = abs(
+            math.atan2(goal_loc[1] - particle_loc[1], goal_loc[0] - particle_loc[0])
+            - math.atan2(
+                coil_locs[i][1] - particle_loc[1], coil_locs[i][0] - particle_loc[0]
+            )
+        )
+        if alpha < angle:
+            angle = alpha
+            min_coil = i
+
+    coil_vals[min_coil] = 1
+
+    return coil_vals
